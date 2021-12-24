1296a09e4d89bbd1c77d4741d11e29c57ab8a75c
dingding-21 <phj8221@naver.com>
2021-12-24 16:31:42 +0900

    [CHORE] modify level2 assignment

--- 1st-seminar/level2.js
+++ 1st-seminar/level2.js
@@ -3,33 +3,33 @@ const soptMem = {
     {
       name: "고성용",
       age: "25",
-      group: "OB",
-      part: "Server",
+      station: "불광",
+      hobby: "축구하는 것",
     },
     {
       name: "조윤서",
       age: "23",
-      group: "OB",
-      part: "Server",
+      station: "백석",
+      hobby: "음악듣는 것",
     },
     {
       name: "김은지",
       age: "24",
-      group: "YB",
-      part: "Server",
+      station: "마두",
+      hobby: "카페가는 것",
     },
     {
       name: "박현지",
       age: "23",
-      group: "OB",
-      part: "Server",
+      station: "이대",
+      hobby: "드라마보는 것",
     },
   ],
 
   introduce: function () {
     this.members.forEach((mem) => {
       console.log(
-        `${mem.name}는 ${mem.age}살이고 ${mem.part}파트이고 ${mem.group}입니다.`
+        `${mem.name}는 나이는 ${mem.age}살이고 집 근처 역은 ${mem.station}역이고 취미는 ${mem.hobby}입니다.`
       );
     });
   },
